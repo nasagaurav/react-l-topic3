8e88829ed73ce922274be8d9df193f4915150579
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-20 08:56:19 +0530

    added new user component after fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
-export default function App() {
+import React, { useState, useEffect } from 'react';
+
+function User(props) {
+  console.log(props);
+  let { item } = props;
+  let { id, name, username, email } = item;
   return (
     <div>
+      {id} {name} {username} {email}
+    </div>
+  );
+}
+
+export default function App() {
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  const [a, seta] = useState([]);
+  const boot = () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((d) => seta(d));
+  };
+  useEffect(boot, []);
+  return (
+    <div>
+      <h1>All users {a.length} </h1>
+      {a.map((item) => (
+        <User item={item} />
+      ))}
     </div>
   );
 }
