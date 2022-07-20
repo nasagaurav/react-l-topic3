88488ff51fdacfc1cd32ea1c83e9c16a3aa1330c
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-20 08:04:58 +0530

    added html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react';
 export default function App() {
+  const url = 'https://api.github.com/users';
   return (
     <div>
+      <h1>all users</h1>
+
+      <div className="flex">
+        <div>
+          <h3>item.login</h3>
+          <img width="100" height="100" src="item.avatar_url" />
+          <a href="item.html_url">view repositry</a>
+        </div>
+      </div>
     </div>
   );
 }
