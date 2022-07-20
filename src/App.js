72d9afe4fdc9a834dc4586137ca70586c1b9c6c5
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-20 08:48:30 +0530

    Text component created with props and children

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-function Text() {
-  return <font>text component</font>;
+function Text(props) {
+  // props matlab properties or attributes
+  console.log('props', props);
+  const color = props.color;
+  const children = props.children;
+  return <font color={color}>{children}</font>;
 }
 
 export default function App() {
-  return <div>
-    <Text/>
-  </div>;
+  return (
+    <div>
+      <Text color="red">tag1 </Text>
+      <Text color="green">tag2 </Text>
+      <Text color="blue">tag3 </Text>
+      <Text color="silver">tag4 </Text>
+    </div>
+  );
 }
