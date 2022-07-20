78f7e642ebaecaa99f4542201c3bf6f461bfdd46
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-20 08:39:23 +0530

    useof useRef completde

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { useRef } from 'react';
 export default function App() {
+  const ref1 = useRef();
+  const ref2 = useRef();
+  const ref3 = useRef();
+  const handleClick = () => {
+    console.log(ref1);
+    console.log(ref2);
+    console.log(ref3);
+    ref1.current.placeholder = 'enter name';
+    ref2.current.placeholder = 'enter email';
+    ref3.current.placeholder = 'enter phone';
+
+    ref1.current.value = 'nasa';
+    ref2.current.value = 'nasa@gaurav.com';
+    ref3.current.value = 9879879877;
+  };
   return (
     <div>
-      <button>click me</button>
+      <input ref={ref1} />
+      <input ref={ref2} />
+      <input ref={ref3} />
+      <button onClick={handleClick}>click me</button>
     </div>
   );
 }
