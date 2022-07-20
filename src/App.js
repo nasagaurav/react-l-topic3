d8182b1807c71d21c95c30763d5040bdb4e5cffd
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-20 08:10:02 +0530

    added get request github and mapped into view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 export default function App() {
   const url = 'https://api.github.com/users';
+  const [a, seta] = useState([]);
+
+  const boot = () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((d) => seta(d));
+  };
+  useEffect(boot, []);
   return (
     <div>
-      <h1>all users</h1>
+      <h1>all users {a.length}</h1>
 
       <div className="flex">
-        <div>
-          <h3>item.login</h3>
-          <img width="100" height="100" src="item.avatar_url" />
-          <a href="item.html_url">view repositry</a>
-        </div>
+        {a.map((item) => (
+          <div className="item">
+            <h3>{item.login}</h3>
+            <img width="100" height="100" src={item.avatar_url} />
+            <br />
+            <a target="_blank" href={item.html_url}>
+              view repositry
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
